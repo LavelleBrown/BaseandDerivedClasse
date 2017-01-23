b46f0dbcd5d51f5ad5249fcf5bfce72d2cb60393
Lavelle Brown <lavellebrown@yahoo.com>
2017-01-23 13:46:11 -0500

    first commit

---
+++ Practice Base and Derived Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Base_and_Derived_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
